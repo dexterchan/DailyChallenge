dd0063780d814933db8f6ecc0ee4d1552ae28a9d
dexterchan <dexterchan@gmail.com>
2019-10-26 17:56:05 +0800

    final

--- OCT2019/findLongestSubStringWithRepeatedCharacters.py
+++ OCT2019/findLongestSubStringWithRepeatedCharacters.py
@@ -8,26 +8,49 @@ class Solution:
     def lengthOfLongestSubstring(self, s):
         # Fill this in.
         print ("Hello")
-        r = self.dummyFindShortestLength(s)
+        #r = self.dummyFindShortestLength(s)
+        r = self.LinearShortestLength(s)
         return r
 
-    def calculateLength ( self, checkArray):
-        return reduce (lambda x, y: x+y, checkArray)
-
+    #O[n^2]
     def dummyFindShortestLength(self, s):
         longestLength = 0
-        for i in range(0, len(s)-1):
+        for i in range(0, len(s)):
             check = [0] * 26
             for chr in s[i:]:
                 inx = self.addr(chr)
                 if(check[inx] == 0):
                     check[inx] = 1
                 else:
-                    segmentLength = self.calculateLength(check)
-                    if(segmentLength > longestLength):
-                        longestLength = segmentLength
                     break
+            segmentLength = reduce (lambda x, y: x+y, check)
+            longestLength = max ( segmentLength, longestLength)
         return longestLength
 
-print (Solution().lengthOfLongestSubstring('abrkaabcdefghijjxxx'))
+    def LinearShortestLength(self,s):
+        longestLength = 0
+        visit = [-1] * 26
+        start=0
+        currentLength = 0
+        for i in range (0,len(s)):
+            chr = s[i]
+            inx = self.addr(chr)
+            if(visit[inx] < start ):
+                visit[inx] = i
+                currentLength = currentLength + 1
+                longestLength = max( longestLength, currentLength)
+            else:
+                start = min(visit[inx]+1,len(s)-1)
+                currentLength=i-start+1
+                visit[inx] = i
+
+        return longestLength,start
+
+longestLen, start = Solution().lengthOfLongestSubstring('abrkaabcdefghijjxxx')
+
+longestLen, start = Solution().lengthOfLongestSubstring('abcdddddfghjkl')
+
+print ("%d %d"%(longestLen,start))
+
+#print (Solution().lengthOfLongestSubstring('abcde'))
 # 10
