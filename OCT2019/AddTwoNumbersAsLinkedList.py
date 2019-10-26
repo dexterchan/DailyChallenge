fc2e0182c40812685d55ce9469e32456c7487898
dexterchan <dexterchan@gmail.com>
2019-10-26 09:56:10 +0800

    first commit

---
+++ OCT2019/AddTwoNumbersAsLinkedList.py
@@ -0,0 +1,56 @@
+# Definition for singly-linked list.
+class ListNode(object):
+  def __init__(self, x):
+    self.val = x
+    self.next = None
+
+class Solution:
+
+  def getNumberFromNodes(self,ll):
+      l = ll
+      lNumber = 0
+      while (l != None):
+          lNumber = l.val + lNumber * 10
+          l = l.next
+      return lNumber
+
+  def addTwoNumbers(self, l1, l2, c = 0):
+    # Fill this in.
+    #Get left number
+    lNumber = self.getNumberFromNodes(l1)
+
+    rNumber = self.getNumberFromNodes(l2)
+
+    calNumber = lNumber + rNumber
+
+    #Parse result
+    remainNumber = calNumber
+    result = None
+    currentNode = None
+    while remainNumber != 0  :
+        digit = remainNumber % 10
+        if result == None :
+            result = ListNode(digit)
+            currentNode = result
+        else:
+            currentNode.next = ListNode (digit)
+            currentNode = currentNode.next
+        remainNumber = remainNumber // 10
+    return result
+
+
+
+
+l1 = ListNode(2)
+l1.next = ListNode(4)
+l1.next.next = ListNode(3)
+
+l2 = ListNode(5)
+l2.next = ListNode(6)
+l2.next.next = ListNode(4)
+
+result = Solution().addTwoNumbers(l1, l2)
+while result:
+  print (result.val, end='')
+  result = result.next
+# 7 0 8
---
+++ OCT2019/findLongestSubStringWithRepeatedCharacters.py
@@ -0,0 +1,7 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s):
+        # Fill this in.
+
+
+print (Solution().lengthOfLongestSubstring('abrkaabcdefghijjxxx'))
+# 10
