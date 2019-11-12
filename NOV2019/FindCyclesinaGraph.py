7896310d15f2e8405e3fd2139c4450471d606b91
dexterchan <dexterchan@gmail.com>
2019-11-12 19:40:16 +0800

    first checkin

---
+++ NOV2019/FindCyclesinaGraph.py
@@ -0,0 +1,15 @@
+#Given an undirected graph, determine if a cycle exists in the graph.
+
+def find_cycle(graph):
+  # Fill this in.
+
+graph = {
+  'a': {'a2':{}, 'a3':{} },
+  'b': {'b2':{}},
+  'c': {}
+}
+print find_cycle(graph)
+# False
+graph['c'] = graph
+print find_cycle(graph)
+# True
