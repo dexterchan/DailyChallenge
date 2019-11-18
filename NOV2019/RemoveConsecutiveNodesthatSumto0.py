5002b15872b5f6f71c3a78aff27f060ceef41e0a
dexterchan <dexterchan@gmail.com>
2019-11-18 23:58:01 +0800

    bug fix RemoveConsecutuveNodes

--- NOV2019/RemoveConsecutiveNodesthatSumto0.py
+++ NOV2019/RemoveConsecutiveNodesthatSumto0.py
@@ -14,32 +14,31 @@ class Node:
         self.next = next
 
 class Solution:
-    def __removeNodesHelper(self, headRef, end):
+    def __removeNodesHelper(self, headRef, end, node):
         if headRef != None:
             headRef.next = end.next
         else:
-            return end.next, None
-        return headRef, headRef
+            return None, end.next, end.next
+        return headRef, headRef.next, node
 
     def removeConsecutiveSumTo0_BrutalForce(self, node):
         checkNode = node
         headRef = None
-        while checkNode is not None:
-            mysum = checkNode.value
 
-            accmnode = checkNode.next
+        while checkNode is not None:
+
+            accmnode = checkNode
+            mysum = 0
             while accmnode is not None:
                 mysum = mysum + accmnode.value
                 if mysum == 0:
-                    checkNode, headRef = self.__removeNodesHelper(headRef, accmnode)
+                    headRef, checkNode, node = self.__removeNodesHelper(headRef, accmnode , node)
                     break
                 accmnode = accmnode.next
+            if mysum != 0:
+                headRef = checkNode
+                checkNode = checkNode.next
 
-
-            headRef = checkNode
-            #else:
-            #    headRef = None
-            checkNode = checkNode.next
         return node
 
 
@@ -62,5 +61,20 @@ if __name__ == "__main__":
     node = removeConsecutiveSumTo0(node)
     while node:
         print (node.value, end=',')
+        node = node.next
+    print ()
+    # 10
+
+
+    node = Node(5)
+    node.next = Node(-3)
+    node.next.next = Node(-3)
+    node.next.next.next = Node(1)
+    node.next.next.next.next = Node (20)
+    node.next.next.next.next.next = Node(4)
+    node.next.next.next.next.next.next = Node(-4)
+    node = removeConsecutiveSumTo0(node)
+    while node:
+        print(node.value, end=',')
         node = node.next
     # 10
