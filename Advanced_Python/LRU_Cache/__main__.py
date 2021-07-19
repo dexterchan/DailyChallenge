0af8d4201b137fde72ab25fb404caed9e7a7954c
dexterchan <dexterchan@gmail.com>
2021-07-19 15:14:25 +0800

    not perfect.. no history

--- Advanced_Python/LRU_Cache/__main__.py
+++ Advanced_Python/LRU_Cache/__main__.py
@@ -1,7 +1,93 @@
 from .cache import LRU, Node
 
 
+def test_nodes():
+    base: Node = Node(base=True)
+    node1: Node = Node(value=1)
+    node2: Node = Node(value=2)
+    node3: Node = Node(value=3)
+    node4: Node = Node(value=4)
+    base.append_node(node1) \
+        .append_node(node2) \
+        .append_node(node3)
+    # Full path
+    print(base)
+
+    # Remove 2
+    node2.remove_node()
+    print(base)
+
+    # Remove 3
+    node3.remove_node()
+    print(base)
+
+    # Remove 1
+    node1.remove_node()
+    print(base)
+
+    base.append_node(node1) \
+        .append_node(node2) \
+        .append_node(node3)
+    print(base)
+
+    # Remove 3 down the list
+    next_node = node3.next_node
+    node3.remove_node()
+    if next_node is not None:
+        next_node.append_node(node3)
+    print(base)
+
+    # Remove 2
+    node2.remove_node()
+    print(base)
+
+    # append 4
+    base.append_node(node4)
+    print(base)
+
+    next_node = node4.next_node
+    node4.remove_node()
+    if next_node is not None:
+        next_node.append_node(node4)
+    print(base)
+
+
 if __name__ == "__main__":
-    ...
-    base: Node = Node()
-    base.append_node(Node(value=1)).append_node(Node(value=2))
+    lru = LRU(max_size=3)
+    lru.put(
+        key="a", value="apple"
+    )
+    lru.put(
+        key="b", value="banana"
+    )
+    print(lru)
+
+    v = lru.get("a")
+    assert v == "apple"
+    print(lru)
+
+    v = lru.get("b")
+    assert v == "banana"
+    print(lru)
+
+    lru.put(
+        key="m", value="melon"
+    )
+    print(lru)
+
+    lru.put(
+        key="k", value="kiwi"
+    )
+    print(lru)
+
+    v = lru.get("k")
+    v = lru.get("k")
+    print(lru)
+    v = lru.get("k")
+    print(lru)
+
+    lru.put("b", "Beetroot")
+    print(lru)
+
+    lru.put("c", "cherry")
+    print(lru)
--- Advanced_Python/LRU_Cache/cache.py
+++ Advanced_Python/LRU_Cache/cache.py
@@ -1,20 +1,19 @@
 from __future__ import annotations
-from typing import Optional
+from typing import Any, Optional, Tuple
 from pydantic import BaseModel
 
 
 class Node(BaseModel):
-    value: int = 0
+    value: str = 0
     prev_node: Optional[Node] = None
     next_node: Optional[Node] = None
+    base: Optional[bool] = False
 
     def append_node(self, node: Node) -> Node:
-        if self.next_node is not None:
-            self.prev_node = node
-
         node.next_node = self.next_node
+        if node.next_node is not None:
+            node.next_node.prev_node = node
         node.prev_node = self
-
         self.next_node = node
 
         return self
@@ -23,7 +22,25 @@ class Node(BaseModel):
         return self.next_node
 
     def remove_node(self) -> None:
-        self.prev_node = self.next_node
+        if self.base:
+            raise "BASE cannot be removed"
+        if self.prev_node is None:
+            return
+        self.prev_node.next_node = self.next_node
+        if self.next_node is not None:
+            self.next_node.prev_node = self.prev_node
+        self.prev_node = None
+        self.next_node = None
+
+    def __str__(self) -> str:
+        value = self.value
+        if self.base:
+            value = "BASE"
+        if self.next_node is not None:
+            return f"{value}->{str(self.next_node)}"
+        else:
+            return f"{value}"
+
 # class Node():
 #     def __init__(self, value:int, prev_node:Node=None, next_node:Node=None) -> None:
 #         self.value = value
@@ -32,7 +49,52 @@ class Node(BaseModel):
 
 
 class LRU:
-    def __init__(self, size: int) -> None:
-        self.size = size
+    def __init__(self, max_size: int) -> None:
+        self.max_size = max_size
         self.my_dict = {}
-        self.priority_lst
+        self.priority_lst: Node = Node(base=True)
+
+    def replace(self, key: str, value: Any):
+        (_, node) = self.my_dict[key]
+        prev_node: Node = node.prev_node
+        new_node: Node = Node(value=key)
+        node.remove_node()
+        prev_node.append_node(new_node)
+        del self.my_dict[key]
+        print(f"{key}:{value}")
+        self.my_dict[key] = (value, new_node)
+        ...
+
+    def put(self, key: str, value: Any):
+        if key in self.my_dict:
+            self.replace(
+                key=key, value=value
+            )
+            return
+        if len(self.my_dict) == self.max_size:
+            redundant_node = self.priority_lst.next()
+            if redundant_node is not None:
+                redundant_node.remove_node()
+                del self.my_dict[redundant_node.value]
+
+        node = Node(value=key)
+        self.my_dict[key] = (value, node)
+        self.priority_lst.append_node(
+            node
+        )
+
+    def get(self, key: str) -> Any:
+        if key not in self.my_dict:
+            raise "Not found"
+        (value, node) = self.my_dict[key]
+        next_node: Node = node.next()
+        if next_node is not None:
+            node.remove_node()
+            next_node.append_node(node=node)
+        return value
+
+    def __str__(self) -> str:
+        m = ""
+        for k, v in self.my_dict.items():
+            m = m + f"{{ {k}:{v[0]} }},"
+        return f" m:{m} inx:{str(self.priority_lst)}"
