91a8874e0e83b538cd330639a683831ba0118190
dexterchan <dexterchan@gmail.com>
2021-09-09 23:50:39 +0800

    add efficient crawling options

--- jupyter/nasdaq_institutional/nasdaq_institutional_analysis.py
+++ jupyter/nasdaq_institutional/nasdaq_institutional_analysis.py
@@ -18,6 +18,17 @@ class Nasdaq_Institution_Page_Parser:
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument('user-agent={0}'.format(user_agent))
+        prefs = {"profile.managed_default_content_settings.images": 2,
+                 "profile.default_content_setting_values.notifications": 2,
+                 "profile.managed_default_content_settings.stylesheets": 2,
+                 "profile.managed_default_content_settings.cookies": 2,
+                 "profile.managed_default_content_settings.javascript": 1,
+                 "profile.managed_default_content_settings.plugins": 1,
+                 "profile.managed_default_content_settings.popups": 2,
+                 "profile.managed_default_content_settings.geolocation": 2,
+                 "profile.managed_default_content_settings.media_stream": 2,
+                 }
+        chrome_options.add_experimental_option("prefs", prefs)
         self.driver = webdriver.Chrome(options=chrome_options)
 
     @backoff.on_exception(
