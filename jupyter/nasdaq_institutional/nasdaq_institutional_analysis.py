a11fbc5dade0ac28703086edf20771a2c1f4837b
dexterchan <dexterchan@gmail.com>
2021-09-10 22:10:34 +0800

    including click to page

--- jupyter/nasdaq_institutional/nasdaq_institutional_analysis.py
+++ jupyter/nasdaq_institutional/nasdaq_institutional_analysis.py
@@ -2,11 +2,26 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import pandas as pd
-from typing import List
+from typing import List, Dict
 from collections import defaultdict
 import backoff
 
 max_trial = 5
+
+
+class Page:
+    def __init__(self, bttn) -> None:
+        self.bttn = bttn
+        self.pagenum = int(bttn.text)
+
+    def distance(self, targetPage: int) -> int:
+        return targetPage - self.pagenum
+
+    @backoff.on_exception(
+        backoff.expo, Exception, max_tries=max_trial, jitter=backoff.full_jitter
+    )
+    def select(self) -> None:
+        self.bttn.click()
 
 
 class Nasdaq_Institution_Page_Parser:
@@ -14,22 +29,23 @@ class Nasdaq_Institution_Page_Parser:
         user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36"
         self.url_template = 'https://www.nasdaq.com/market-activity/stocks/{stock}/institutional-holdings'
         chrome_options = Options()
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-dev-shm-usage")
+        # chrome_options.add_argument("--headless")
+        # chrome_options.add_argument("--no-sandbox")
+        # chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument('user-agent={0}'.format(user_agent))
-        prefs = {"profile.managed_default_content_settings.images": 2,
-                 "profile.default_content_setting_values.notifications": 2,
-                 "profile.managed_default_content_settings.stylesheets": 2,
-                 "profile.managed_default_content_settings.cookies": 2,
-                 "profile.managed_default_content_settings.javascript": 1,
-                 "profile.managed_default_content_settings.plugins": 1,
-                 "profile.managed_default_content_settings.popups": 2,
-                 "profile.managed_default_content_settings.geolocation": 2,
-                 "profile.managed_default_content_settings.media_stream": 2,
-                 }
-        chrome_options.add_experimental_option("prefs", prefs)
+        # prefs = {"profile.managed_default_content_settings.images": 2,
+        #          "profile.default_content_setting_values.notifications": 2,
+        #          "profile.managed_default_content_settings.stylesheets": 2,
+        #          "profile.managed_default_content_settings.cookies": 2,
+        #          "profile.managed_default_content_settings.javascript": 1,
+        #          "profile.managed_default_content_settings.plugins": 1,
+        #          "profile.managed_default_content_settings.popups": 2,
+        #          "profile.managed_default_content_settings.geolocation": 2,
+        #          "profile.managed_default_content_settings.media_stream": 2,
+        #          }
+        # chrome_options.add_experimental_option("prefs", prefs)
         self.driver = webdriver.Chrome(options=chrome_options)
+
         self.page = 1
 
     def close(self) -> None:
@@ -42,17 +58,65 @@ class Nasdaq_Institution_Page_Parser:
     def load_details(self, security: str, page: int) -> pd.DataFrame:
         self.driver.get(
             f'https://www.nasdaq.com/market-activity/stocks/{security}/institutional-holdings')
-        html = self.driver.page_source
-        soup = BeautifulSoup(html)
-        column_names = self._list_column_names(soup=soup)
 
-        num_of_pages = self._get_number_of_pages(soup=soup)
+        page_details = self._load_soup()
+
+        num_of_pages = page_details["num_of_pages"]
+
+        if page < 1 and page > num_of_pages:
+            raise Exception(f"Page should be between {1} and {num_of_pages}")
+
+        if self.page != page:
+            self._go_to_page(page_num=page)
+            page_details = self._load_soup()
+
+        soup = page_details["soup"]
+        column_names = page_details["columns"]
 
         df: pd.DataFrame = self._load_to_dataframe(
             soup=soup, column_list=column_names
         )
+        self.page = self._get_current_page()
 
         return df
+
+    def _load_soup(self) -> Dict:
+        html = self.driver.page_source
+        soup = BeautifulSoup(html)
+        column_names = self._list_column_names(soup=soup)
+        num_of_pages = self._get_number_of_pages(soup=soup)
+
+        return {
+            "soup": soup,
+            "columns": column_names,
+            "num_of_pages": num_of_pages
+        }
+
+    def _go_to_page(self, page_num: int) -> None:
+        self.page = self._get_current_page()
+        print(f"We are at page {self.page}, load page {page_num}")
+        if self.page == page_num:
+            return
+
+        pageList: List[Page] = []
+        for bttn in self.driver.find_elements_by_tag_name("button.pagination__page"):
+            pageList.append(Page(bttn=bttn))
+
+        closestPage: Page = None
+        page_distance = 100000
+        for p in pageList:
+            abs_distance = abs(p.distance(page_num))
+            if abs_distance < page_distance:
+                closestPage = p
+                page_distance = abs_distance
+
+        print(
+            f"Going to press {closestPage.pagenum} with distance {page_distance}")
+        # Press the button
+        if closestPage is not None:
+            closestPage.select()
+
+        return self._go_to_page(page_num=page_num)
 
     def _list_column_names(self, soup) -> List[str]:
         # list the columns name
