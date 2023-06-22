dca1c0b141dbae0408b271216da8fc605678716e
dexterchan <dexterchan@gmail.com>
2023-06-22 20:25:55 +0800

    add practise

---
+++ Practise/linkedlist.py
@@ -0,0 +1,145 @@
+from __future__ import annotations
+import io
+
+class DblLinkedList:
+    def __init__(self) -> None:
+        self._head:Node = None
+        self._tail:Node = None
+        self._size:int = 0
+        self._cur:Node = None
+        pass
+
+    def __len__(self) -> int:
+        return self._size
+    
+    def __repr__(self) -> str:
+        return self._head
+
+    def __iter__(self) -> Node:
+        self._cur = self._head
+        return self._cur
+    
+    def __next__(self) -> Node:
+        if self._cur is None:
+            raise StopIteration
+        self._cur = self._cur.next
+        return self._cur
+    
+    def append(self, data) -> Node:
+        
+        if self._head is None:
+            self._head = Node(data)
+            self._tail = self._head
+            self._size += 1
+            return self._head
+        else:
+            return self._tail.append(data)
+    
+    
+    
+    pass
+
+
+class Node:    
+    def __init__(self, data):
+        self.data = data
+        self.next:Node = None
+        self.prev:Node = None
+    
+    # def __iter__(self):
+    #     return self
+    
+    # def __next__(self):
+    #     if self.next is None:
+    #         raise StopIteration
+    #     return self.next
+    
+    def __repr__(self) -> str:
+        n:Node = self
+        str_io = io.BytesIO()
+        while n is not None:
+            str_io.write(f"{n.data},".encode("utf-8"))
+            n = n.next    
+        return str_io.getvalue().decode("utf-8")
+    
+    def insert(self, new_node:Node) ->tuple[Node, bool] :
+        new_node.next = self
+        new_node.prev = self.prev
+        is_first:bool = self.prev is None
+        #check if it is first element
+        if not is_first:
+            self.prev.next = new_node
+            
+        self.prev = new_node
+        return new_node, is_first
+        
+    def delete(self) -> tuple[Node, bool, bool]:
+        #check if it is first element
+        is_first = self.prev is None
+        is_last = self.next is None
+        if is_first:
+            self.next.prev = None
+            return self.next, is_first, is_last
+        #check if it is not last element
+        elif is_last:
+            self.next.prev = self.prev
+        self.prev.next = self.next
+
+        return self.prev, is_first, is_last
+    
+    def append(self, new_node:Node) -> Node:
+        new_node.next = self.next
+        new_node.prev = self
+        if self.next is not None:
+            self.next.prev = new_node
+        self.next = new_node
+        return new_node
+
+    def find(self, value) -> Node:
+        n:Node = self
+        while n is not None:
+            if n.data == value:
+                return n
+            n = n.next
+        return None
+
+if __name__ == "__main__":
+    lead:Node = None
+    a_node = Node("a")
+    lead = a_node
+
+    b_node, is_first = a_node.insert(Node("b"))
+    print(b_node, is_first)
+    lead = b_node if is_first else lead
+
+
+    c_node, is_first = b_node.insert(Node("c"))
+    print(c_node, is_first)
+    lead = c_node if is_first else lead
+
+    d_node, is_first = b_node.insert(Node("d"))
+    print(d_node, is_first)
+    lead = d_node if is_first else lead
+
+    print(lead)
+
+    d_node.delete()
+    print(lead)
+
+    f_node = lead.find("b")
+    print(f_node.prev.data)
+
+    new_node, is_first, is_last = f_node.delete()
+    lead = new_node if is_first else lead
+    print(lead)
+
+    g_node = c_node.append(Node("g"))
+    print(lead)
+    # print(g_node.prev.data)
+    # print(g_node.next.data)
+    # print(g_node.next.prev.data)
+    # for element in lead:
+    #     print(element.data)
+
+    
+
